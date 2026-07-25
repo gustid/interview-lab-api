d793d217a6764a8d5e55fb5b94f99f28ce7ee427
Augustin Dinulica <dinulicaaugustin@gmail.com>
2026-07-25 14:17:36 +0300

    chore: adds knex

---
+++ knexfile.ts
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+import type { Knex } from 'knex';
+
+const config: Record<string, Knex.Config> = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './src/database/migrations',
+      extension: 'ts',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './src/database/seeds',
+      extension: 'ts',
+    },
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.TEST_DATABASE_URL,
+    migrations: {
+      directory: './src/database/migrations',
+      extension: 'ts',
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './dist/database/migrations',
+      extension: 'js',
+      tableName: 'knex_migrations',
+    },
+  },
+};
+
+export default config;
